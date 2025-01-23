419d4abfd92bb0548283d1ef98758c0b9b9bc6d5
parmisvazifeh <parmisvaziferm@gmail.com>
2025-01-23 05:58:30 -0800

    Add: Code generation(imports)

--- Code/deepDSLCodeGenerator.py
+++ Code/deepDSLCodeGenerator.py
@@ -3,3 +3,12 @@ class DeepDSLCodeGenerator:
         self.non_operands = []
         self.operand_stack = []
         self.code_stack = []
+
+
+    def getImportHeader(self):
+        result = ( "import tensorflow as tf\n"
+                   "import numpy as np\n"
+                   "import matplotlib.pyplot as plt\n"
+                   "from tensorflow.keras.models import Sequential\n"
+                   "from tensorflow.keras.layers import Flatten, Dense\n")
+        return result
