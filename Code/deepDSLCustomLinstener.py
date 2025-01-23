ff2b96bceaa8bcca4fd7399d3b27f5811a414283
parmisvazifeh <parmisvaziferm@gmail.com>
2025-01-23 12:15:25 -0800

    Add: added custom listener

--- Code/deepDSLCustomLinstener.py
+++ Code/deepDSLCustomLinstener.py
@@ -1,10 +1,12 @@
 from Repository.ast import AST
 from Repository.make_ast_subtree import make_ast_subtree
 from gen.deepDSLListener import deepDSLListener
+from gen.deepDSLParser import deepDSLParser
+
 
 class DeepDSLCustomListener(deepDSLListener):
     def __init__(self, rule_names):
-        self.overridden_rules = ['layer', 'types', 'units', 'activation']
+        self.overridden_rules = ['layer', 'types', 'units','training', 'activation','visualize', 'input_shape', 'metric_choice' ,'epoch']
         self.rule_names = rule_names
         self.ast = AST()
 
@@ -24,3 +26,20 @@ class DeepDSLCustomListener(deepDSLListener):
 
     def exitActivation(self, ctx):
         make_ast_subtree(self.ast, ctx, "activation", keep_node=True)
+
+    def exitTraining(self, ctx):
+        make_ast_subtree(self.ast, ctx, "training", keep_node=True)
+
+    def exitVisualize(self, ctx):
+        make_ast_subtree(self.ast, ctx, "visualize", keep_node=True)
+
+    def exitInput_shape(self, ctx:deepDSLParser.Input_shapeContext):
+        make_ast_subtree(self.ast, ctx, "input_shape", keep_node=True)
+
+    def exitMetric_choice(self, ctx:deepDSLParser.Input_shapeContext):
+        make_ast_subtree(self.ast, ctx, "metric_choice", keep_node=True)
+
+    def exitEpochs(self,ctx):
+        make_ast_subtree(self.ast, ctx, ctx.getChild(2).getText())
+
+
