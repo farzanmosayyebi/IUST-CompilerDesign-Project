f0a9430637c940798189c7c5de6b86204b2b8cad
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2025-01-26 01:22:34 +0330

    refactor: modify input and output file paths

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ def main(args):
 
 if __name__ == '__main__':
 	argparser = argparse.ArgumentParser()
-	argparser.add_argument('-n', '--file', help='Input source', default=r'input.txt')
-	argparser.add_argument('-o', '--output', help='Output path', default=r'test_output.txt')
+	argparser.add_argument('-n', '--file', help='Input source', default=r'input.deep')
+	argparser.add_argument('-o', '--output', help='Output path', default=r'output.py')
 	args = argparser.parse_args()
 	main(args)
 
