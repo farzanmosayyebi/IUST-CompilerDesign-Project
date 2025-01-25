4fe876334c49e477e5ed7e1489236450bc63a44d
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2025-01-26 02:21:05 +0330

    chore: generated output

--- src/output/generated_output.py
+++ src/output/generated_output.py
@@ -8,3 +8,16 @@ class NewNetwork:
 		self.model.add(tf.keras.layers.Dense(units=256, activation='Relu', input_shape=(28, 28)))
 		self.model.add(tf.keras.layers.Dense(units=128, input_shape=(28, 28)))
 		self.model.add(tf.keras.layers.Dense(units=10, activation='Softmax'))
+
+	def compile_model(self):
+		self.model.compile(optimizer='adam',
+			loss='SparseCategoricalCrossentropy',
+			metrics = ['loss','accuracy'])
+
+	def train_model(self, x_train, y_train, x_val, y_val):
+		self.model.fit(
+			x_train, y_train,
+			validation_data=(x_val, y_val),
+			epochs=10,
+			batch_size=32
+		)
