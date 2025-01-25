3c5906dbbfacf02e87a6d387561306b014836fa5
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2025-01-26 01:20:17 +0330

    refactor: generate activation

--- Code/deepDSLCodeGenerator.py
+++ Code/deepDSLCodeGenerator.py
@@ -2,16 +2,18 @@ class DeepDSLCodeGenerator:
     def __init__(self):
         self.non_operands = [
             'layer',
-            'training',
-            'visualize',
+            'activation',
             'input_shape',
-            'evaluate',
+            'training',
             'dataset',
+            'visualize',
+            'evaluate',
         ]
 
         self.operand_stack = []
         self.code_stack = []
         self.shape_stack = []
+        self.aux_stack = []
 
     def is_operand(self, item):
         return item not in self.non_operands
@@ -19,7 +21,7 @@ class DeepDSLCodeGenerator:
     @staticmethod
     def generate_initial(self):
         imports = "import tensorflow as tf\n" + \
-                  "import numpy as np\n"
+                  "import numpy as np\n\n\n"
 
         class_def = f"class NewNetwork:\n" + \
                     f"\tdef __init__(self):\n" + \
@@ -39,7 +41,7 @@ class DeepDSLCodeGenerator:
             if code is not None:
                 result += code
 
-        with open("output.txt", "w") as f:
+        with open("generated_output.py", "w") as f:
             f.write(self.generate_initial(self))
             f.write(result)
         return result
@@ -47,6 +49,8 @@ class DeepDSLCodeGenerator:
     def generate_code_based_on_non_operand(self, item):
         if item == "layer":
             self.generate_layer()
+        elif item == "activation":
+            self.generate_activation()
         elif item == "input_shape":
             self.generate_input_shape()
 
@@ -54,16 +58,24 @@ class DeepDSLCodeGenerator:
         y = self.operand_stack.pop()
         x = self.operand_stack.pop()
         code_string = f"input_shape=({x}, {y})"
-        self.shape_stack.append(code_string)
+        self.aux_stack.append(("input_shape", code_string))
 
     def generate_layer(self):
-        layer_activation = self.operand_stack.pop()
         layer_units = self.operand_stack.pop()
         layer_type = self.operand_stack.pop()
 
         layer_input_shape = ''
-        if len(self.shape_stack) > 0:
-            layer_input_shape = f', {self.shape_stack.pop()}'
+        if len(self.aux_stack) > 0 and self.aux_stack[-1][0] == "input_shape":
+            layer_input_shape = f', {self.aux_stack.pop()[1]}'
 
-        layer_def = f"\t\tself.model.add(tf.keras.layers.{layer_type}(units={layer_units}, activation=\'{layer_activation}\'{layer_input_shape}))\n"
+        layer_activation = ''
+        if len(self.aux_stack) > 0 and self.aux_stack[-1][0] == "activation":
+            layer_activation = f', {self.aux_stack.pop()[1]}'
+
+        layer_def = f"\t\tself.model.add(tf.keras.layers.{layer_type}(units={layer_units}{layer_activation}{layer_input_shape}))\n"
         self.code_stack.append(layer_def)
+
+    def generate_activation(self):
+        activation = self.operand_stack.pop()
+        code_string = f"activation=\'{activation}\'"
+        self.aux_stack.append(("activation", code_string))
