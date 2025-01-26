e4a91fa7e03b27a4216f2147bfbd4025096601e9
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2025-01-26 21:59:26 +0330

    chore: output

--- src/output/generated_output.py
+++ src/output/generated_output.py
@@ -5,10 +5,9 @@ import numpy as np
 class NewNetwork:
 	def __init__(self):
 		self.model = tf.keras.Sequential()
-		self.model.add(tf.keras.layers.Dense(units=512, activation='Relu', input_shape=(64, 64)))
-		self.model.add(tf.keras.layers.Dense(units=256, activation='Tanh'))
-		self.model.add(tf.keras.layers.Dense(units=128, activation='Sigmoid'))
-		self.model.add(tf.keras.layers.Dense(units=5, activation='Softmax'))
+		self.model.add(tf.keras.layers.Dense(units=256, activation='Relu', input_shape=(28, 28)))
+		self.model.add(tf.keras.layers.Dense(units=128, input_shape=(28, 28)))
+		self.model.add(tf.keras.layers.Dense(units=10, activation='Softmax'))
 
 	def compile_model(self):
 		self.model.compile(optimizer='loss',
@@ -19,6 +18,13 @@ class NewNetwork:
 		self.model.fit(
 			x_train, y_train,
 			validation_data=(x_val, y_val),
-			epochs=50,
-			batch_size=16
+			epochs=10,
+			batch_size=32
 		)
+
+	def visualize_model(self, x, y):
+		print(f"visualizing model on a {x} * {y} grid.")
+
+	def evaluate_model(self, x_test, y_test):
+		results = self.model.evaluate(x_test, y_test, verbose=0)
+		print(f"Evaluation results:\n {results}")
